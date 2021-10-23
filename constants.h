dafe3d48c7137a6f468681bd0f3b3021370d848c
shevby <bohdan.u.shevchenko@gmail.com>
2021-10-23 13:42:41 +0300

    add:final BiomCell struct

--- constants.h
+++ constants.h
@@ -5,58 +5,82 @@
 #include <fstream>
 
 
+
 namespace Cinder {
-  enum class MapTypes {
-    WORLD_MAP,
-    LOCATION_MAP
-  };
+    enum class MapTypes {
+        WORLD_MAP,
+        LOCATION_MAP
+    };
 
-  struct Map {
-    MapTypes mapType;
-    uint32_t width;
-    uint32_t height;
-    std::vector<std::vector<uint8_t>> map;
-    void save(std::string filePath) {
-        using namespace std;
-        ofstream outFile(filePath, ios::out|ios::binary);
-        outFile.write((char*)&mapType, sizeof(MapTypes));
-        outFile.write((char*)&width, sizeof(width));
-        outFile.write((char*)&height, sizeof(height));
+    enum class RiverDiraction {
+        NO_RIVER = 0,
+        TOP_DOWN,
+        DOWN_TOP = TOP_DOWN,
+        TOP_LEFT,
+        LEFT_TOP = TOP_LEFT,
+        TOP_RIGHT,
+        RIGHT_TOP = TOP_RIGHT,
+        LEFT_RIGHT,
+        RIGHT_LEFT = LEFT_RIGHT,
+        RIGHT_DOWN,
+        DOWN_RIGHT = RIGHT_DOWN,
+        LEFT_DOWN,
+        DOWN_LEFT = LEFT_DOWN
+    };
 
-        for(auto & row : map) {
-            outFile.write((char*)row.data(), width);
+    struct BiomCell {
+        uint8_t tile: 5;
+        uint8_t river: 3; //RiverDiraction
+    };
+
+    struct Map {
+        MapTypes mapType;
+        uint32_t width;
+        uint32_t height;
+
+        std::vector<std::vector<BiomCell>> map;
+
+        void save(std::string filePath) {
+            using namespace std;
+            ofstream outFile(filePath, ios::out|ios::binary);
+            outFile.write((char*)&mapType, sizeof(MapTypes));
+            outFile.write((char*)&width, sizeof(width));
+            outFile.write((char*)&height, sizeof(height));
+
+            for(auto & row : map) {
+                outFile.write((char*)row.data(), width);
+            }
+
+            outFile.flush();
+            outFile.close();
+
         }
+    };
 
-        outFile.flush();
-        outFile.close();
+    enum class Biomes {
+        WATER = 0,
+        FIELD,
+        FOREST,
+        ROCK,
+        HIGH_ROCK,
+        GLACIER,
+        SWAMP,
+        DESERT,
+        SAVANNA,
+    };
 
-    }
-  };
-
-  enum class Biomes {
-      WATER = 0,
-      FIELD,
-      FOREST,
-      ROCK,
-      HIGH_ROCK,
-      GLACIER,
-      SWAMP,
-      DESERT,
-      SAVANNA,
-  };
-
-  enum class LocationTiles {
-      WATER = 0,
-      GRAVEL,
-      GROUND,
-      GRASS,
-      SAND,
-      ROCK,
-      LAVA,
-      ICE,
-      SNOW,
-      DIRT
-  };
+    enum class LocationTiles {
+        WATER = 0,
+        GRAVEL,
+        GROUND,
+        GRASS,
+        SAND,
+        ROCK,
+        LAVA,
+        ICE,
+        SNOW,
+        DIRT
+    };
 
     enum class NaturalObjects {
 
@@ -103,30 +127,30 @@ namespace Cinder {
         ORES_END
     };
 
-  enum class TileBorder {
-      NONE = 0,
-      LEFT,
-      RIGHT,
-      TOP,
-      BOTTOM
-  };
+    enum class TileBorder {
+        NONE = 0,
+        LEFT,
+        RIGHT,
+        TOP,
+        BOTTOM
+    };
 
-  struct Biom {
-      Biomes biomType;
-      bool hasSnow;
-      //uint8 hasRiver;
-      TileBorder riverEntry;
-      TileBorder riverExit;
+    struct Biom {
+        Biomes biomType;
+        bool hasSnow;
+        //uint8 hasRiver;
+        TileBorder riverEntry;
+        TileBorder riverExit;
 
-      Biom(Biomes biomType){
-          this->biomType = biomType;
-      }
-  };
+        Biom(Biomes biomType){
+            this->biomType = biomType;
+        }
+    };
 
-  struct Village {
-      float x;
-      float y;
-      uint8_t population;
-  };
+    struct Village {
+        float x;
+        float y;
+        uint8_t population;
+    };
 } // namespace Cinder
 
